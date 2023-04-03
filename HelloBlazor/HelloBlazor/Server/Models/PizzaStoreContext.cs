c4e73d2ed59689702b2196538023e66a1a93544d
Ger Toad <theyayo47@gmail.com>
2023-04-02 21:18:58 -0600

    Blazor intro

---
+++ HelloBlazor/HelloBlazor/Server/Models/PizzaStoreContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using HelloBlazor.Shared;
+
+namespace HelloBlazor.Server.Models
+{
+    public class PizzaStoreContext
+    {
+    }
+}
+public class PizzaStoreContext : DbContext
+{
+    public DbSet<PizzaSpecial> Specials { get; set; }
+    public PizzaStoreContext(DbContextOptions options)
+    : base(options)
+    {
+    }
+}
---
+++ HelloBlazor/HelloBlazor/Server/Program.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.DependencyInjection;
+using HelloBlazor.Server.Models;
+//using System.Linq; // imported as a sugestion 
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<PizzaStoreContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("PizzaStore")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+var ScopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
+
+using (var Scope = ScopeFactory.CreateScope())
+{
+    var Context = Scope.ServiceProvider
+    .GetRequiredService<PizzaStoreContext>();
+    if (!Context.Specials.Any())
+    {
+        SeedData.Initialize(Context);
+    }
+}
+
+app.Run();
